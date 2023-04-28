d44e665c98f2fe23f439515b741fb6cf0aca289c
GabitJunior <gabit.omarov@gmail.com>
2023-04-29 00:01:09 +0600

    v.10

--- contracts/KZC.sol
+++ contracts/KZC.sol
@@ -8,6 +8,7 @@ contract KZC is ERC20 {
     constructor() ERC20("KZC wrapped", "KZC") {}
 
     function mint(address addr, uint256 amount) external {
+        require(msg.sender == address(0x00C5d1B9a29232738b5aF5028bb230519D513Da8), "You aren't the owner");
         _mint(addr, amount*10**_decimals);
     }
 
