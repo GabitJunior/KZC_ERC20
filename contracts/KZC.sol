c46879ece9717ed4de99048cf786357e790a8857
GabitJunior <gabit.omarov@gmail.com>
2023-04-29 21:45:33 +0600

    v.11

--- contracts/KZC.sol
+++ contracts/KZC.sol
@@ -5,10 +5,14 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract KZC is ERC20 {
     uint8 _decimals = 8;
-    constructor() ERC20("KZC wrapped", "KZC") {}
+    address payable public owner;
+
+    constructor() ERC20("KZC wrapped", "KZC") {
+        owner = payable(msg.sender);
+    }
 
     function mint(address addr, uint256 amount) external {
-        require(msg.sender == address(0x00C5d1B9a29232738b5aF5028bb230519D513Da8), "You aren't the owner");
+        require(msg.sender == owner, "You aren't the owner");
         _mint(addr, amount*10**_decimals);
     }
 
