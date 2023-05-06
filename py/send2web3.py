ec5f76203361230ad7c5fe6b99afb07246955c54
GabitJunior <gabit.omarov@gmail.com>
2023-05-06 12:29:51 +0600

    v.23

--- py/send2web3.py
+++ py/send2web3.py
@@ -7,6 +7,7 @@ import logging.config
 import logging
 from send_message import message_dev, message_user
 from config import wallet_url, wallet_url_kzc, wallet_url_ltc, my_log_ini_file, dev_chat_id
+import asyncio
 
 
 logging.config.fileConfig(my_log_ini_file, disable_existing_loggers=False)
@@ -97,13 +98,10 @@ ERC20_ABI = json.loads('''[
 ''')
 
 load_dotenv()
-#Load the private keys
 PRIVATE_KEY = os.environ.get("PRIVATE_KEY")
 
-print(PRIVATE_KEY)
-
 # KZC токен
-kzc_contract_address = '0x7FdA984F8Fe8244b1336aadfBd9cAe486C56CFeC'
+kzc_contract_address = os.environ.get("CONTRACT_ADDRESS")
 
 # инициализация контракта KZC
 kzc_contract = web3.eth.contract(kzc_contract_address, abi=ERC20_ABI)
@@ -114,44 +112,64 @@ kzc_decimals = kzc_contract.functions.decimals().call()
 
 acct=web3.eth.account.from_key(PRIVATE_KEY)
 #print(F"current signer={acct.address}")
-
 s_exp = "https://testnet.bscscan.com/tx/{0}"
 
 dict_transaction = {
-    'chainId': web3.eth.chain_id, 
-    'gas': 210000, 
+    'chainId': web3.eth.chain_id,
+    'gas': 210000,
     'gasPrice': web3.eth.gas_price,
-    'nonce': web3.eth.getTransactionCount(mykzc_addr),
+    'nonce': web3.eth.getTransactionCount(acct.address)
 }
 
-cursor = sql_db().cursor
-s_sql="SELECT pm.id, pm.chat_id, pm.wallet_from, pm.wallet_to, pm.amount "
-s_sql=s_sql+" from payments as pm  "
-s_sql=s_sql+" where pm.status = -2 "
-s_sql=s_sql+" order by 1"
+#web3.eth.getTransactionCount(mykzc_addr)
 
-try:
-  cursor.execute(s_sql)
-except pymysql.Error as e:
-  logger.error(e)
-  logger.error(s_sql)
-  message_dev(s_sql)
-result = cursor.fetchall()
-if len(result) > 0:
+
+async def main():
+	cursor = sql_db().cursor
+	s_sql="SELECT pm.id, pm.chat_id, pm.wallet_from, pm.wallet_to, pm.amount "
+	s_sql=s_sql+" from payments as pm  "
+	s_sql=s_sql+" where pm.status = -2 "
+	s_sql=s_sql+" order by 1"
+
+	try:
+		cursor.execute(s_sql)
+		result = cursor.fetchall()
+	except pymysql.Error as e:
+		logger.error(e)
+		logger.error(s_sql)
+		message_dev(s_sql)
+		result = ""
+
+	if len(result) > 0:
 		for r_rec in result:
-			id_pay = r_rec[0]             # ID записи
-			chat_id = r_rec[1]            # чат_ид пользователя
-			wallet_from = r_rec[2]			  # кошелек откуда списывать
-			wallet_to = r_rec[3]			    # кошелек на какой перевести
-			amount = r_rec[4]			        # сумма перевода
+			id_pay = r_rec[0]			# ID записи
+			chat_id = r_rec[1]			# чат_ид пользователя
+			wallet_from = r_rec[2]			# кошелек откуда списывать
+			wallet_to = r_rec[3]			# кошелек на какой перевести
+			amount = r_rec[4]			# сумма перевода
+			amount = amount * pow(10 , 8)
+			amnt_int = int(amount)
+			#amnt_int = 65
 			checksum_addr = Web3.toChecksumAddress(wallet_to)
-			mint = kzc_contract.functions.mint(checksum_addr, amount).buildTransaction(dict_transaction)
+			mint = kzc_contract.functions.mint(checksum_addr, amnt_int).buildTransaction(dict_transaction)
 			# Подписываем
 			signed_txn = web3.eth.account.signTransaction(mint, PRIVATE_KEY)
 			# Отправляем
 			txn_hash = web3.eth.sendRawTransaction(signed_txn.rawTransaction)
-			sql_db().update_withdraw_bsc(id_pay, txn_hash)
-			s_str = s_exp.format(txn_hash)
+			#print(f"txn_hash={txn_hash}")
+			#if txn_hash[:2] == '0x':
+			#	txn_hash = txn_hash[2:]
+			txn_str = txn_hash.hex()
+			print(f"txn_str={txn_str}")
+			sql_db().update_withdraw_bsc(id_pay, txn_str)
+			s_str = s_exp.format(txn_str)
 			message_dev(s_str)
 			message_user(s_str, chat_id)
+	else:
+		print("Not found req to BSC")
 
+#asyncio.run(main())
+
+if __name__ ==  '__main__':
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(main())
